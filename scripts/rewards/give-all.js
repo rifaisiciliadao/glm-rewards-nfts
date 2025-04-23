3d70d9b74c0ec87d4e665ac2b60d835b90c13adb
turinglabs <seb@turinglabs.org>
2025-04-23 14:48:43 +0200

    Enhance reward distribution script by adding nonce handling and improved logging. Adjusted beneficiary eligibility criteria and gas price calculation for NFT rewards.

--- scripts/rewards/give-all.js
+++ scripts/rewards/give-all.js
@@ -15,16 +15,20 @@ async function main() {
     const rewardList = [];
     for (let i = 0; i < epochList.length; i++) {
         const beneficiary = epochList[i].split(',')[0]
-        if (beneficiary.indexOf('0x') === 0 && rewarded < 100) {
+        if (beneficiary.indexOf('0x') === 0 && (parseInt(epochList[i].split(',')[1]) >= 100) || (epochList[i].split(',')[1].indexOf('e') !== -1)) {
             rewardList.push(beneficiary);
             try {
                 rewarded++;
+                console.log("Number of rewards:", rewarded)
                 const nftRewarded = await contract.nftRewarded(beneficiary)
                 if (!nftRewarded) {
                     console.log("Rewarding user:", beneficiary)
-                    const gasPrice = await provider.getGasPrice()
+                    const nonce = await provider.getTransactionCount(wallet.address)
+                    console.log("Nonce:", nonce)
+                    const gasPrice = (await provider.getGasPrice())
+                    const gasPriceBoost = gasPrice.div(100).mul(5)
                     console.log("Gas price:", gasPrice.toString())
-                    const tx = await contract.giveRewardNFT(beneficiary, { gasPrice: gasPrice })
+                    const tx = await contract.giveRewardNFT(beneficiary, { gasPrice: gasPrice.add(gasPriceBoost), nonce: nonce })
                     console.log("Waiting for transaction to be confirmed...", tx.hash)
                     const receipt = await tx.wait()
                     console.log("Gas used:", receipt.gasUsed.toString())
@@ -33,7 +37,7 @@ async function main() {
                     console.log("User already rewarded:", beneficiary)
                 }
             } catch (e) {
-                console.log("Error rewarding user:", beneficiary)
+                console.log("Error rewarding user:", beneficiary, e.message)
             }
         } else {
             console.log("Skipping user:", beneficiary, "staking balance:", epochList[i].split(',')[1])
